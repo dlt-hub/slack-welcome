b6cd52fa702860ebf8db52f209693cae7539d426
hulmanaseer00 <163604758+hulmanaseer00@users.noreply.github.com>
2024-10-18 17:49:00 +0200

    separate block for icp

--- helpers/message_event.py
+++ helpers/message_event.py
@@ -149,6 +149,7 @@ def handle_message_event(ack, body, botclient, bqclient):
 
             enrichment_pipeline.run([enrichment_data], table_name="enriched_users")
 
+    if channel_id in ["C07R1362X0D"]: #annuns-debug-channel
 
         message_ts = body['event'].get('thread_ts', None)
 
@@ -179,7 +180,7 @@ def handle_message_event(ack, body, botclient, bqclient):
 
                     message_link = f"https://dlthub-community.slack.com/archives/{channel_id}/p{message_ts.replace('.', '')}"
                     botclient.chat_postMessage(
-                        channel="C07R1362X0D", #anuuns-debug-channel
+                        channel="C07R1362X0D", #community_support-channel
                         text=f"An ICP just sent a message, you might want to reply to <{message_link}|this message>...\n\n" + conditions_met 
                     )
 
